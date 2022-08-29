b61861b01e781033cb1c8c109834a82ce8baa511
scosta <salcosta@protonmail.com>
2022-08-29 10:56:52 -0400

    added collapsibles removed email

--- salcosta.js
+++ salcosta.js
@@ -214,3 +214,19 @@ document.addEventListener('keydown', function(event) {
     }
 });
 
+// Collapsible
+
+var collapse = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < collapse.length; i++) {
+  collapse[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
